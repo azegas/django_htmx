c78f4d5063df89edbe6b388921fd44598a3b5339
Arvydas Gasparavicius <arvydas.gasparavicius@seb.se>
2024-02-22 10:58:17 +0200

    verbose names in models

--- example/models.py
+++ example/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
+
     def __str__(self):
         return self.name
 
@@ -11,6 +15,10 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=100)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
 
     def __str__(self):
         return self.name
