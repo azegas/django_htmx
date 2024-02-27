152e919ac12d0fff487eaebb45423ad830597241
s2753g <arvydas.gasparavicius@seb.se>
2024-02-27 22:06:23 +0200

    added additional fields

--- example/models.py
+++ example/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 class Country(models.Model):
     name = models.CharField(max_length=100)
+    pollution = models.DecimalField(
+        max_digits=19,
+        decimal_places=2,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Country"
@@ -16,6 +22,12 @@ class City(models.Model):
     name = models.CharField(max_length=100)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     inhabitants = models.DecimalField(
+        max_digits=19,
+        decimal_places=2,
+        blank=True,
+        null=True,
+    )
+    growth = models.DecimalField(
         max_digits=19,
         decimal_places=2,
         blank=True,
